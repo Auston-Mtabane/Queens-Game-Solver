e4c6e72c2fee8bc71c22c32c9b0bf09a427037d8
Auston Mtabane <mtabane.auston@gmail.com>
2025-06-22 21:09:06 +0200

    added turtle display

--- solver.py
+++ solver.py
@@ -1,4 +1,30 @@
 from grid import Grid
+import turtle as t
+def gridToTurtle(grid:Grid):
+    s = grid.getSize*50
+    half = s/2
+    t.setup(s+20,s+20)
+    t.speed(-1)
+    t.hideturtle()
+    t.colormode(255)
+
+
+    for i,row in enumerate(grid.grid):
+        for j,b in enumerate(row):
+            
+            r,g,b = b.getColor
+            t.fillcolor(r,g,b)
+            t.teleport(-half+50*j,half -50*i)
+            t.begin_fill()
+            for _ in range(4):
+                t.forward(50)
+                t.right(90)
+            t.end_fill()
+
+    t.done()
+
+    
+    
 
 game = Grid("level2")
 
@@ -6,5 +32,6 @@ for r in range(game.getSize):
     for c in range(game.getSize):
         game.setQueenAt(r,c)
 
+gridToTurtle(game)
 
 game.printGrid()
